87b2375f8703633b2f1473a6e4b67f553289af72
gtHu <1650435737@qq.com>
2020-06-08 23:25:31 +0800

    练习

--- spring-framework-5.0.x/spring-context/src/main/java/org/springframework/context/support/AbstractRefreshableConfigApplicationContext.java
+++ spring-framework-5.0.x/spring-context/src/main/java/org/springframework/context/support/AbstractRefreshableConfigApplicationContext.java
@@ -78,6 +78,8 @@ public abstract class AbstractRefreshableConfigApplicationContext extends Abstra
 			Assert.noNullElements(locations, "Config locations must not be null");
 			this.configLocations = new String[locations.length];
 			for (int i = 0; i < locations.length; i++) {
+				//解析给定路径
+				//125行
 				this.configLocations[i] = resolvePath(locations[i]).trim();
 			}
 		}
